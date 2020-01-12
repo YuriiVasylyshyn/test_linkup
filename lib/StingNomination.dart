4500ed25ca97c37ec72393fcbe638374ef2e47dc
Yurii Vasylyshyn <vasylyshyn.yura.19@gmail.com>
2020-01-12 19:47:23 +0200

    12.01.20

--- lib/StingNomination.dart
+++ lib/StingNomination.dart
@@ -23,6 +23,19 @@ class _StingNominationState extends State<StingNomination> {
     this.randomnum = randNum(5);
   }
 
+  List Players = [
+    {"id": 1, "name": "Alan", "img": "img1.png"},
+    {"id": 2, "name": "Alan", "img": "img1.png"},
+    {"id": 3, "name": "Alan", "img": "img1.png"},
+    {"id": 4, "name": "Alan", "img": "img1.png"},
+    {"id": 5, "name": "Alan", "img": "img1.png"},
+    {"id": 6, "name": "Alan", "img": "img1.png"},
+    {"id": 7, "name": "Alan", "img": "img1.png"},
+    {"id": 8, "name": "Alan", "img": "img1.png"},
+    {"id": 9, "name": "Alan", "img": "img1.png"},
+    {"id": 10, "name": "Alan", "img": "img1.png"}
+  ];
+
   void votefunc() {
     setState(() {
       this.vote = !this.vote;
@@ -32,203 +45,230 @@ class _StingNominationState extends State<StingNomination> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/bg2.jpg'),
-              fit: BoxFit.cover,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/bg2.jpg'),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-      ListView(
-        children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+          ListView(
             children: <Widget>[
-              Row(
+              Column(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
-                  Image(
-                    image: AssetImage('icons/menu.png'),
-                    height: 45,
-                    width: 45,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Image(
+                        image: AssetImage('icons/menu.png'),
+                        height: 45,
+                        width: 45,
+                      ),
+                      Image(
+                        image: AssetImage('icons/street.png'),
+                        height: 45,
+                        width: 45,
+                      ),
+                      Image(
+                        image: AssetImage('icons/car.png'),
+                        height: 45,
+                        width: 45,
+                      ),
+                      Image(
+                        image: AssetImage('icons/appartment.png'),
+                        height: 45,
+                        width: 45,
+                      ),
+                      Image(
+                        image: AssetImage('icons/plate.png'),
+                        height: 45,
+                        width: 45,
+                      ),
+                      Image(
+                        image: AssetImage('icons/hotel.png'),
+                        height: 45,
+                        width: 45,
+                      ),
+                      Image(
+                        image: AssetImage('icons/docks.png'),
+                        height: 45,
+                        width: 45,
+                      ),
+                    ],
                   ),
-                  Image(
-                    image: AssetImage('icons/street.png'),
-                    height: 45,
-                    width: 45,
+                  Container(
+                    margin: EdgeInsets.all(3),
+                    child:Text(
+                    "ROUND 2: AUTOMOBILE",
+                    style: TextStyle(
+                      color: Color.fromRGBO(206, 17, 65, 1),
+                      fontFamily: 'PaybAck',
+                      fontSize: 30,
+                    ),
+                  ),),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(60, 0, 60, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Image(
+                          image: AssetImage('icons/redcircle.png'),
+                          height: 20,
+                          width: 20,
+                        ),
+                        Image(
+                          image: AssetImage('icons/redcircle.png'),
+                          height: 20,
+                          width: 20,
+                        ),
+                        Image(
+                          image: AssetImage('icons/whitecircle.png'),
+                          height: 20,
+                          width: 20,
+                        ),
+                        Image(
+                          image: AssetImage('icons/blackcircle.png'),
+                          height: 20,
+                          width: 20,
+                        ),
+                        Image(
+                          image: AssetImage('icons/blackcircle.png'),
+                          height: 20,
+                          width: 20,
+                        ),
+                      ],
+                    ),
                   ),
-                  Image(
-                    image: AssetImage('icons/car.png'),
-                    height: 45,
-                    width: 45,
+                 Container( 
+                   margin: EdgeInsets.all(3),
+                   child: Text(
+                    "STING NOMINATION",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'PaybAck',
+                      fontSize: 30,
+                    ),
+                  ),),
+                  RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'PaybAck',
+                        fontSize: 30,
+                      ),
+                      children: [
+                        TextSpan(text: "NOMINATE "),
+                        TextSpan(
+                          text: '$randomnum',
+                          style: TextStyle(
+                            color: Color.fromRGBO(206, 17, 65, 1),
+                          ),
+                        ),
+                        TextSpan(text: " PLAYERS"),
+                      ],
+                    ),
                   ),
-                  Image(
-                    image: AssetImage('icons/appartment.png'),
-                    height: 45,
-                    width: 45,
+                  VoteButton(
+                    vote: vote,
+                    onPressed: votefunc,
+                    name: "Alan",
+                    img: 'assets/img1.png',
                   ),
-                  Image(
-                    image: AssetImage('icons/plate.png'),
-                    height: 45,
-                    width: 45,
+                  VoteButton(
+                    vote: vote,
+                    onPressed: votefunc,
+                    name: "Alan",
+                    img: 'assets/img1.png',
                   ),
-                  Image(
-                    image: AssetImage('icons/hotel.png'),
-                    height: 45,
-                    width: 45,
+                  VoteButton(
+                    vote: vote,
+                    onPressed: votefunc,
+                    name: "Alan",
+                    img: 'assets/img1.png',
                   ),
-                  Image(
-                    image: AssetImage('icons/docks.png'),
-                    height: 45,
-                    width: 45,
+                  VoteButton(
+                    vote: vote,
+                    onPressed: votefunc,
+                    name: "Alan",
+                    img: 'assets/img1.png',
+                  ),
+                  VoteButton(
+                    vote: vote,
+                    onPressed: votefunc,
+                    name: "Alan",
+                    img: 'assets/img1.png',
+                  ),
+                  VoteButton(
+                    vote: vote,
+                    onPressed: votefunc,
+                    name: "Alan",
+                    img: 'assets/img1.png',
+                  ),
+                  VoteButton(
+                    vote: vote,
+                    onPressed: votefunc,
+                    name: "Alan",
+                    img: 'assets/img1.png',
+                  ),
+                  VoteButton(
+                    vote: vote,
+                    onPressed: votefunc,
+                    name: "Alan",
+                    img: 'assets/img1.png',
+                  ),
+                  VoteButton(
+                    vote: vote,
+                    onPressed: votefunc,
+                    name: "Alan",
+                    img: 'assets/img1.png',
+                  ),
+                  VoteButton(
+                    vote: vote,
+                    onPressed: votefunc,
+                    name: "Alan",
+                    img: 'assets/img1.png',
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(7),
+                    height: 60.0,
+                    width: 274.0,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(123, 7, 37, 1),
+                          spreadRadius: 1,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => VoteScreen()),
+                        );
+                      },
+                      child: Text(
+                        'Nominate',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontFamily: 'Typewriter',
+                          color: Colors.white,
+                        ),
+                      ),
+                      color: Color.fromRGBO(206, 17, 65, 1),
+                    ),
                   ),
                 ],
-              ),
-              Text(
-                "ROUND 2: AUTOMOBILE",
-                style: TextStyle(
-                  color: Color.fromRGBO(206, 17, 65, 1),
-                  fontFamily: 'PaybAck',
-                  fontSize: 30,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(60, 0, 60, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Image(
-                      image: AssetImage('icons/redcircle.png'),
-                      height: 20,
-                      width: 20,
-                    ),
-                    Image(
-                      image: AssetImage('icons/redcircle.png'),
-                      height: 20,
-                      width: 20,
-                    ),
-                    Image(
-                      image: AssetImage('icons/whitecircle.png'),
-                      height: 20,
-                      width: 20,
-                    ),
-                    Image(
-                      image: AssetImage('icons/blackcircle.png'),
-                      height: 20,
-                      width: 20,
-                    ),
-                    Image(
-                      image: AssetImage('icons/blackcircle.png'),
-                      height: 20,
-                      width: 20,
-                    ),
-                  ],
-                ),
-              ),
-              Text(
-                "STING NOMINATION",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'PaybAck',
-                  fontSize: 30,
-                ),
-              ),
-              RichText(
-                text: TextSpan(
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'PaybAck',
-                    fontSize: 30,
-                  ),
-                  children: [
-                    TextSpan(text: "NOMINATE "),
-                    TextSpan(
-                      text: '$randomnum',
-                      style: TextStyle(
-                        color: Color.fromRGBO(206, 17, 65, 1),
-                      ),
-                    ),
-                    TextSpan(text: " PLAYERS"),
-                  ],
-                ),
-              ),
-              VoteButton(
-                vote: vote,
-                onPressed: votefunc,
-              ),
-              VoteButton(
-                vote: vote,
-                onPressed: votefunc,
-              ),
-              VoteButton(
-                vote: vote,
-                onPressed: votefunc,
-              ),
-              VoteButton(
-                vote: vote,
-                onPressed: votefunc,
-              ),
-              VoteButton(
-                vote: vote,
-                onPressed: votefunc,
-              ),
-              VoteButton(
-                vote: vote,
-                onPressed: votefunc,
-              ),
-              VoteButton(
-                vote: vote,
-                onPressed: votefunc,
-              ),
-              VoteButton(
-                vote: vote,
-                onPressed: votefunc,
-              ),
-              VoteButton(
-                vote: vote,
-                onPressed: votefunc,
-              ),
-              VoteButton(
-                vote: vote,
-                onPressed: votefunc,
-              ),
-              
-              Container(
-                height: 60.0,
-                width: 274.0,
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color.fromRGBO(123, 7, 37, 1),
-                      spreadRadius: 1,
-                      offset: Offset(0, 2),
-                    ),
-                  ],
-                ),
-                child: RaisedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => VoteScreen()),
-                    );
-                  },
-                  child: Text(
-                    'Nominate',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontFamily: 'Typewriter',
-                      color: Colors.white,
-                    ),
-                  ),
-                  color: Color.fromRGBO(206, 17, 65, 1),
-                ),
               ),
             ],
           ),
         ],
-      ),],),
+      ),
     );
   }
 }
--- lib/button.dart
+++ lib/button.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 class VoteButton extends StatelessWidget {
-  VoteButton({this.vote, @required this.onPressed});
+  VoteButton({this.vote, @required this.onPressed, this.img, this.name});
   final vote;
   final Function onPressed;
+  final img;
+  final name;
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 274,
       height: 60,
+      margin: EdgeInsets.all(7),
       decoration: BoxDecoration(
         boxShadow: [
           BoxShadow(
@@ -25,14 +28,28 @@ class VoteButton extends StatelessWidget {
           color: vote ? Colors.white : Color.fromRGBO(82, 82, 82, 1),
           size: 30,
         ),
-        label: Text(
-          'New Game',
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.white,
-          ),
+        label: Row(
+          children: [
+            Image(
+              image: AssetImage(img),
+              width: 45,
+              height: 45,
+            ),
+            Padding(
+              padding: EdgeInsets.all(5),
+              child: Text(
+                name,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
         ),
-        color: vote ? Color.fromRGBO(0, 73, 144, 1) : Color.fromRGBO(42, 42, 42, 1),
+        color: vote
+            ? Color.fromRGBO(0, 73, 144, 1)
+            : Color.fromRGBO(42, 42, 42, 1),
       ),
     );
   }
